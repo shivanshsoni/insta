3621f34c730af109f89ac47426c72a694167d1a8
shivanshsoni <shivanshsoni23@gmail.com>
2018-03-25 13:26:29 +0530

    Changes

--- app/src/main/java/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/Utils/BottomNavigationViewHelper.java
@@ -4,21 +4,13 @@ package Utils;
  * Created by shivansh on 25/3/18.
  */
 
-import android.util.Log;
-
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
-
-/**
- * Created by User on 5/28/2017.
- */
 
 public class BottomNavigationViewHelper {
 
     private static final String TAG = "BottomNavigationViewHel";
 
     public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
-        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
-        bottomNavigationViewEx.enableAnimation(false);
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
